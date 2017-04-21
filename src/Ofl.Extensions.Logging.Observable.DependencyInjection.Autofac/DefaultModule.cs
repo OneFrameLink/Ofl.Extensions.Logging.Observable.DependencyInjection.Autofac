720bea6928144b583e67651a91907ad73fea54f6
casperOne <casperOne@caspershouse.com>
2017-04-21 13:17:32 -0400

    Initial code push.

---
+++ src/Ofl.Extensions.Logging.Observable.DependencyInjection.Autofac/DefaultModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Autofac;
+
+namespace Ofl.Extensions.Logging.Observable.DependencyInjection.Autofac
+{
+    public class DefaultModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Validate parameters.
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            // Register.
+            builder.RegisterType<LogObservableFactory>().As<ILogObservableFactory>();
+        }
+    }
+}
